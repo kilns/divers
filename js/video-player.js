061e374307872dbd36c602d42683fc5a7ceffc02
kilns <121528759+kilns@users.noreply.github.com>
2022-12-28 18:30:06 +0100

    fdfg

--- js/video-player.js
+++ js/video-player.js
@@ -1,8 +1,14 @@
 (function () {
 	'use strict';
 
+	var iframe = document.getElementsByTagName('iframe')[0];
+iframe.style.background = 'white';
+iframe.contentWindow.document.body.style.backgroundColor = 'white';
+
 	// Does the browser actually support the video element?
 	var supportsVideo = !!document.createElement('video').canPlayType;
+	
+
 
 	if (supportsVideo) {
 		// Obtain handles to main elements
